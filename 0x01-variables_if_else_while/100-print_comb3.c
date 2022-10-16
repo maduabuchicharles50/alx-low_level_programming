e6226a4614b2e9a33bd2bef81a2171b95f8a4af6
mbonu-charles <saintabuchi50@gmail.com>
2022-10-16 07:17:44 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -28,13 +28,4 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
-	}
-
-	putchar('\n');
-
-	return (0);
-}
-			                                                                                                       
-														
-							
-							                                                                                                                                    
+	}							                                                                                                                                    
