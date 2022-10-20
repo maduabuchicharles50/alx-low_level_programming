8d33746927cab185cae40d67121759df39ab4754
mbonu-charles <saintabuchi50@gmail.com>
2022-10-20 14:28:42 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * Function - Print that checks for uppercase
+ *   return 1 if c is uppercase and 0 otherwise
+ *
+ * Return: Always 1.
+ */
+
+int _isupper(int c)
+{
+	 if (c >= 'A' && c <= 'Z')
+	 {
+		  return (1);
+	}	
+	 else
+	   {
+		     return (0); 
+	   }
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef  MAIN
+#define  MAIN
+
+/*
+ * File main.h
+ * auth: tibaut
+ * desc: Header file containing declarations for all functions
+ *
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+
+
+
+#endif
