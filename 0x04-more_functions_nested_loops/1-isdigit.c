d57c7b80f2429312837e451e3b78b88770e4abc8
mbonu-charles <saintabuchi50@gmail.com>
2022-10-21 07:14:31 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * function - Prints for a digit
+ *   return 1 if c is a digit
+ *
+ *   return 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
