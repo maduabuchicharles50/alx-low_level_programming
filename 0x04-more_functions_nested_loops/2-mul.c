c82e52e34b19edee61cf4518b7b6ade6a5c9765f
mbonu-charles <saintabuchi50@gmail.com>
2022-10-21 16:00:22 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * function - Print multiplies two intgers.
+ * Return: Success 1.
+ */
+
+int mul(int a, intb)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
+
