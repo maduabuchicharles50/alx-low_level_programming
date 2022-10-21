9f0d378bf7608de60b81fd111580ccd1fae32904
mbonu-charles <saintabuchi50@gmail.com>
2022-10-21 05:35:16 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,13 @@
 
 /**
  * main - tests function that printd if integer is positive or negative
+ *
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 
-	i = 98;
 	positive_or_negative(i);
 
 	return (0);
