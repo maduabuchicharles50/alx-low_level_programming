3dc57b9c60c3fd24f99f30867bc6a0bd16dd8323
mbonu-charles <saintabuchi50@gmail.com>
2022-10-21 04:02:39 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,16 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
-	
-	/* 	while (i < 0)
-	   	{
-	   		putchar(i);
-	   	} */
-	
+
+	/*
+	 *   while (i < 0)
+	  {
+		 putchar(i);
+	   }
+	  */
+
 	printf("Infinitte loop avoided! \\o/\n");
 
 	return (0);
