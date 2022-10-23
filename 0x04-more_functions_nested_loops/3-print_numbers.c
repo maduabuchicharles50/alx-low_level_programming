675cc2ab5b5ded68b4f79cff6b26e3fe7291d49c
mbonu-charles <saintabuchi50@gmail.com>
2022-10-23 16:38:46 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints out numbers from 0 - 9
+ *
+ * Return: result
+ */
+void print_numbers(void)
+{
+	char result = '0';
+
+	while (result <= '9')
+	{
+		_putchar(result);
+
+		result++;
+	}
+	_putchar('\n');
+
+}
