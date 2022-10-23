41a2a40bc1d363fe0058e5a34d1f9e01a2a07ae5
mbonu-charles <saintabuchi50@gmail.com>
2022-10-23 15:33:43 -0700

    main.h added

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,19 +1,25 @@
-#ifndef  MAIN
-#define  MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
-/*
- * File main.h
- * auth: tibaut
- * desc: Header file containing declarations for all functions
- *
- */
+#include <stdio.h>
+#include <ctype.h>
 
+/* Prototype for print_most_numbers in 4-print_most_numbers.c */
+void print_most_numbers(void);
+
+/* Prototype for _putchar function in _putchar.c */
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+
+/* Prototype for print_numbers function in 3-main.c */
 void print_numbers(void);
 
+/* Prototype for mul function in mul-main.c task */
+int mul(int a, int b);
 
+/* Prototype for _isdigit function in 1-main.c task */
+int _isdigit(int c);
 
-#endif
+/* Prototype for _isupper function in 0-main.c task */
+int _isupper(int c);
+
+#endif /* MAIN_H */
