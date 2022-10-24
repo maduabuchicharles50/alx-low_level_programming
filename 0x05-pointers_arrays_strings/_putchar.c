126f3a7ef39d6d0e4ea1c32d0e786255afacfe7e
mbonu-charles <saintabuchi50@gmail.com>
2022-10-24 14:32:02 -0700

    _putchar.c commit

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - print the charater c on stdout
+ * @c: print the charater
+ *
+ * Return: 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
