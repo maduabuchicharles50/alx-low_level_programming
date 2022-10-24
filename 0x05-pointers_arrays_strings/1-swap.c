bf35fac81fd3d78d79d527df74611aeb89f47168
mbonu-charles <saintabuchi50@gmail.com>
2022-10-24 13:35:16 -0700

    1-swap.c commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of two nums
+ * @b : first pointer
+ * @a : second pointer
+ * Returnn : void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int constant = *a;
+	*b = *a;
+	*a = constant;
+}
