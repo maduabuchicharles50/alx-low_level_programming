3bae50dce22484ba9a287907e388c454468ad765
mbonu-charles <saintabuchi50@gmail.com>
2022-10-25 14:28:45 -0700

    2-strlen.c committed

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string.
+ * @str: string.
+ *
+ * Return: length.
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (*str++)
+	{
+		len++;
+	}
+
+	return (len);
+}
