6bcc2f9e76ed4175c3f33d9694a592ca74e440a5
mbonu-charles <saintabuchi50@gmail.com>
2022-10-25 21:27:21 -0700

    8-print_array.c updated

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,10 @@ void print_array(int *a, int n)
 
 	for (t = 0; t < n; t++)
 	{
-		putchar("%d", a[t]);
+		printf("%d", a[t]);
 		if (t != n - 1)
-			putchar(",");
+			printf(", ");
 	}
 
-	 putchar("\n");
+	 printf("\n");
 }
