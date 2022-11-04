f2d5f40811907a841b159a1205735433cd99f14e
mbonu-charles <saintabuchi50@gmail.com>
2022-11-04 10:26:19 -0700

    0-whatsmyname.c first commit

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+#include <unistd.h>
+
+/**
+ * _putchar - Prints the character c to stdout
+ * @c: print the character
+ * Return: on success 1.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
