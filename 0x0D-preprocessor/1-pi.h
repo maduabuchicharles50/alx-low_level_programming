87f2550a8c7018a68500dbd01e096173f8b77643
Mbonu Charles <saintabuchi50@gmail.com>
2022-11-16 02:44:37 +0000

    1-pi.h first commit update

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,13 +1,13 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef PI_H
+#define PI_H
 
 /*
- * File: 0-object_like_macro.h
+ * File: 1-pi.h
  * Auth: Brennan D Baraban
- * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
  */
 
-#define SIZE 1024
+#define PI 3.14159265359
 
 #endif
